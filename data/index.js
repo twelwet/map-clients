e0e03a13a8cf6cb81dd70f79a237b6858501e7d6
twelwet <akimovmax@mail.ru>
2022-05-30 12:45:28 +0300

    add row/ivn.json

--- data/index.js
+++ data/index.js
@@ -5,6 +5,6 @@ const objects = require(`./json/objects.json`);
 const backboneNet = require(`./json/fiber-lines-backbone.json`);
 const cityNet = require(`./json/fiber-lines-city-net.json`);
 const jkhNet = require(`./json/fiber-lines-jkh-net.json`);
-const ivn = require(`./json/ivn.json`);
+const rawIvnData = require(`./raw/ivn.json`);
 
-module.exports = {nodes, objects, backboneNet, cityNet, jkhNet, ivn};
+module.exports = {nodes, objects, backboneNet, cityNet, jkhNet, rawIvnData};
