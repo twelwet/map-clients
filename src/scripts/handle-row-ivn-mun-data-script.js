7c46acf80879da63ba953f62dcc51a0a5577689d
twelwet <akimovmax@mail.ru>
2022-05-31 21:58:44 +0300

    temporary script for ivn-mun cameras

---
+++ src/scripts/handle-row-ivn-mun-data-script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const {rawIvnMunData} = require(`../../data`);
+const {saveToFile, getCsvFromJson} = require(`../utils`);
+const {FilePath} = require(`../constants`);
+
+const DEFAULT_FIELDS = [`node_id`, `name`, `address`, `description`, `latitude`, `longitude`, `node_distance`, `cost`];
+
+const pointsRawData = rawIvnMunData[`features`];
+
+const rawPoints = pointsRawData.filter((item) => item[`geometry`][`type`] === `Point`);
+
+const cameras = rawPoints.map((item, index) => ({
+  [`node_id`]: `?`,
+  name: `ИВН-МУН-${index + 1}`,
+  address: ``,
+  description: `${item[`properties`][`name`]}`,
+  latitude: item[`geometry`][`coordinates`][1],
+  longitude: item[`geometry`][`coordinates`][0],
+  [`node_distance`]: `?`,
+  cost: `?`,
+}));
+
+saveToFile(FilePath.IvnMunCameras.CSV, getCsvFromJson(cameras, DEFAULT_FIELDS))
+  .then(() => console.log(`Operation is successful.`));
