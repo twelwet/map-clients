f5755e5e7b064d1cd64c5f6d8555d73ce21f49b7
twelwet <akimovmax@mail.ru>
2022-06-06 13:26:43 +0300

    filter backboneLayer for LineStrings only

--- data/index.js
+++ data/index.js
@@ -2,7 +2,7 @@
 
 const nodes = require(`./json/nodes.json`);
 const objects = require(`./json/objects.json`);
-const backboneNet = require(`./json/fiber-lines-backbone.json`);
+const backboneNetRaw = require(`./json/fiber-lines-backbone.json`);
 const cityNet = require(`./json/fiber-lines-city-net.json`);
 const jkhNet = require(`./json/fiber-lines-jkh-net.json`);
 const ivnBoxes = require(`./json/ivn-boxes.json`);
@@ -10,6 +10,14 @@ const ivnCameras = require(`./json/ivn-cameras.json`);
 const ivnMunCameras = require(`./json/ivn-mun-cameras.json`);
 const rawIvnData = require(`./raw/ivn.json`);
 const rawIvnMunData = require(`./raw/ivn-mun.json`);
+
+const backboneNetLineStrings = backboneNetRaw[`features`].filter((item) => item[`geometry`][`type`] === `LineString`);
+
+const backboneNet = {
+  type: backboneNetRaw[`type`],
+  metadata: backboneNetRaw[`metadata`],
+  features: backboneNetLineStrings,
+};
 
 module.exports = {
   nodes,
