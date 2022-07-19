b0a51d948a4947f825bbae3ac8242bf903246825
twelwet <akimovmax@mail.ru>
2022-07-19 15:49:19 +0300

    add pages-data for server

--- src/server/constants.js
+++ src/server/constants.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const {ivnMunCost, trafficLightsData} = require(`../../data`);
-const {placesByPriority, priorityNames} = require(`./prepared-fvf-data`);
-
 const Title = {
   BACKBONE: `Магистраль`,
   CLIENTS: `Клиенты`,
@@ -35,58 +32,4 @@ const Link = {
   TRAFFIC_LIGHTS: `/traffic_lights`,
 };
 
-const backbone = {
-  title: Title.BACKBONE,
-  mapScript: MapScript.BACKBONE,
-  link: Link.BACKBONE,
-  menuName: Title.BACKBONE,
-  pageName: PageName.BACKBONE,
-  data: {},
-};
-
-const clients = {
-  title: Title.CLIENTS,
-  mapScript: MapScript.CLIENTS,
-  link: Link.CLIENTS,
-  menuName: Title.CLIENTS,
-  pageName: PageName.CLIENTS,
-  data: {},
-};
-
-const ivn = {
-  title: Title.IVN,
-  mapScript: MapScript.IVN,
-  link: Link.IVN,
-  menuName: Title.IVN,
-  pageName: PageName.IVN,
-  data: {
-    munCostData: ivnMunCost,
-  },
-};
-
-const fvf = {
-  title: Title.FVF,
-  mapScript: MapScript.FVF,
-  link: Link.FVF,
-  menuName: Title.FVF,
-  pageName: PageName.FVF,
-  data: {
-    placesByPriority,
-    priorityNames,
-  },
-};
-
-const trafficLights = {
-  title: Title.TRAFFIC_LIGHTS,
-  mapScript: MapScript.TRAFFIC_LIGHTS,
-  link: Link.TRAFFIC_LIGHTS,
-  menuName: Title.TRAFFIC_LIGHTS,
-  pageName: PageName.TRAFFIC_LIGHTS,
-  data: {
-    trafficLightsData,
-  },
-};
-
-const pages = [backbone, clients, ivn, fvf, trafficLights];
-
-module.exports = {pages};
+module.exports = {Title, PageName, MapScript, Link};
--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@
 require(`dotenv`).config();
 const express = require(`express`);
 const path = require(`path`);
-const {pages} = require(`./constants`);
+const {pages} = require(`./pages-data`);
 const {getHeaderMenu, errorHandler} = require(`./utils`);
 const PORT = process.env.SERVER_PORT;
 
